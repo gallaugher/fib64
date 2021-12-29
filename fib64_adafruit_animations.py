8f886fe49548a34ef62be51289e54df4727f1116
Gallaugher <john.gallaugher@bc.edu>
2021-12-29 15:20:52 -0500

    Uploaded Two Files

---
+++ fib64_adafruit_animations.py
@@ -0,0 +1,90 @@
+# Lesson 8 - adafruit_led_animation
+# Lesson 7 - Working with an LED Light Strip
+import board
+import neopixel
+import time
+from adafruit_led_animation.animation.blink import Blink
+from adafruit_led_animation.animation.colorcycle import ColorCycle
+from adafruit_led_animation.animation.chase import Chase
+from adafruit_led_animation.animation.comet import Comet
+from adafruit_led_animation.animation.pulse import Pulse
+from adafruit_led_animation.animation.sparkle import Sparkle
+from adafruit_led_animation.animation.SparklePulse import SparklePulse
+from adafruit_led_animation.animation.rainbow import Rainbow
+from adafruit_led_animation.animation.rainbowchase import RainbowChase
+from adafruit_led_animation.sequence import AnimationSequence
+
+from adafruit_led_animation.color import (
+    AMBER, #(255, 100, 0)
+    AQUA, # (50, 255, 255)
+    BLACK, #OFF (0, 0, 0)
+    BLUE, # (0, 0, 255)
+    CYAN, # (0, 255, 255)
+    GOLD, # (255, 222, 30)
+    GREEN, # (0, 255, 0)
+    JADE, # (0, 255, 40)
+    MAGENTA, #(255, 0, 20)
+    OLD_LACE, # (253, 245, 230)
+    ORANGE, # (255, 40, 0)
+    PINK, # (242, 90, 255)
+    PURPLE, # (180, 0, 255)
+    RED, # (255, 0, 0)
+    TEAL, # (0, 255, 120)
+    WHITE, # (255, 255, 255)
+    YELLOW, # (255, 150, 0)
+    RAINBOW # a list of colors to cycle through
+    # RAINBOW is RED, ORANGE, YELLOW, GREEN, BLUE, and PURPLE ((255, 0, 0), (255, 40, 0), (255, 150, 0), (0, 255, 0), (0, 0, 255), (180, 0, 255))
+)
+
+INDIGO = (63, 0, 255)
+VIOLET = (127, 0, 255)
+
+colors = [RED, MAGENTA, ORANGE, YELLOW, GREEN, JADE, BLUE, INDIGO, VIOLET, PURPLE, BLACK]
+
+pixels_pin = board.MOSI
+pixels_num_of_lights = 64
+pixel_brightness = 0.2
+pixels = neopixel.NeoPixel(pixels_pin, pixels_num_of_lights, brightness = pixel_brightness, auto_write=True)
+
+blink = Blink(pixels, speed=0.5, color=AMBER)
+
+colorcycle = ColorCycle(pixels, 0.1, colors=colors)
+
+chase = Chase(pixels, speed=0.1, color=WHITE, size=3, spacing=6)
+
+comet = Comet(pixels, speed=0.1, color=RED, tail_length=int(pixels_num_of_lights/4), bounce=True)
+
+pulse = Pulse(pixels, speed=0.05, color=AMBER, period=2)
+
+sparkle = Sparkle(pixels, speed=0.05, color=PURPLE)
+
+sparkle_pulse = SparklePulse(pixels, speed=0.05, period=5, color=BLUE)
+
+rainbow = Rainbow(pixels, speed=0.05, period=2)
+
+rainbow_chase = RainbowChase(pixels, speed=0.01, size=5, spacing=0, step=8)
+
+animations = AnimationSequence(
+    blink, colorcycle, chase, comet, pulse, sparkle, sparkle_pulse, rainbow, rainbow_chase, advance_interval=5, auto_clear=True
+)
+
+while True:
+    #blink.animate()
+    #blink_strip.animate()
+    #colorcycle.animate()
+    #colorcycle_strip.animate()
+    #chase.animate()
+    #chase_strip.animate()
+    #comet.animate()
+    #comet_strip.animate()
+    #pulse.animate()
+    #pulse_strip.animate()
+    #sparkle.animate()
+    #sparkle_strip.animate()
+    #sparkle_pulse.animate()
+    #sparkle_pulse_strip.animate()
+    #rainbow.animate()
+    #rainbow_strip.animate()
+    #rainbow_chase.animate()
+    #rainbow_chase_strip.animate()
+    animations.animate()
---
+++ simple_rainbow_spiral_demo.py
@@ -0,0 +1,24 @@
+import board, time, neopixel, digitalio, touchio
+from rainbowio import colorwheel
+
+num_of_leds = 64
+led_pin = board.MOSI
+led_brightness = 0.2
+leds = neopixel.NeoPixel(led_pin, num_of_leds, brightness=led_brightness, auto_write=False)
+leds.fill((0, 0, 255))
+
+while True:
+    for i in range(num_of_leds):
+        leds[i] = colorwheel(i*4)
+        leds.write()
+        time.sleep(0.01)
+    for i in range (led_brightness*100, -1, -1):
+        leds.brightness = i/100
+        leds.write()
+        time.sleep(0.01)
+    leds.brightness = led_brightness
+    leds.fill((0, 0, 0))
+    leds.write()
+
+
+
